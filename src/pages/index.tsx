f56f15d5f512c7c365cf2fc3759b5e136b4718c8
FrankBatista1 <fb53901@gmail.com>
2023-03-30 10:21:26 -0400

    auth added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,16 @@
 import { type NextPage } from "next";
 import Image from "next/image";
+import { SignIn, useUser } from "@clerk/nextjs";
 
 const Home: NextPage = () => {
+
 
   return (
     <>
       <section className="flex min-h-screen flex-col">
         <main className="flex grow">
-          <article className="mt-[32px] flex w-[100%] flex-row justify-center pb-[32px]">
-            <div className="m-4 flex max-w-[350px] grow flex-col justify-center">
+          <article className="mt-[32px] flex w-[100%] flex-row justify-center pb-[32px] gap-10">
+            <div className="hidden m-4 lg:flex max-w-[400px] grow flex-col justify-center">
               <Image
                 src="/assets/laptop-blackscreen.png"
                 alt="laptop"
@@ -16,36 +18,28 @@ const Home: NextPage = () => {
                 height={500}
               />
             </div>
-            <div className="flex max-w-[350px] grow flex-col justify-center">
-              <div className="border-x-grey-800 mt-[10px] flex flex-col items-center justify-center border border-solid pb-[10px]">
-                <div className="mt-[16px] self-center">
-                  <Image
-                    src="/assets/logo.png"
-                    alt="communicate-logo"
-                    width={150}
-                    height={150}
-                  />
-                </div>
-                <form className="mt-[24px] flex w-[70%] flex-col gap-[6px]">
-                  <input className="border border-solid border-slate-300 bg-slate-100 pl-[4px] pb-[4px] pt-[4px] text-xs focus:outline-none"></input>
-                  <input className="border border-solid border-slate-300 bg-slate-100 pl-[4px] pb-[4px] pt-[4px] text-xs focus:outline-none"></input>
-                  <button className="mt-2 rounded-md bg-sky-400 p-[2px] text-sm font-semibold text-white opacity-[.7]">
-                    Sign in
-                  </button>
-                  <div className="flex flex-row items-center gap-[12px]">
-                    <div className="h-[1px] grow bg-gray-800 opacity-[.3]"></div>
-                    <span>or</span>
-                    <div className="h-[1px] grow bg-gray-800 opacity-[.3]"></div>
-                  </div>
-                  <span className="self-center font-semibold">
-                    Log in with discord
-                  </span>
-                  <span className="self-center text-sm">Forgot password?</span>
-                </form>
-              </div>
-              <div className="border-x-grey-800 mt-[10px] flex flex-col items-center justify-center border border-solid p-[10px]">
-                <span>Dont have an acocunt? Sign up</span>
-              </div>
+            <div className="m-10 flex max-w-[350px] flex-col justify-center">
+               <SignIn appearance={
+                {
+                  elements: {
+                    logoBox: "self-center w-[150px] h-[150px]",
+                    headerTitle: "hidden",
+                    headerSubtitle: "hidden",
+                    card: "shadow-none border border-solid border-slate-300",
+                    formFieldInput: "border border-solid border-slate-300 bg-slate-100",
+                    socialButtonsBlockButton: "border border-solid border-slate-300",
+                    footer: "self-center",
+                  },
+                  variables: {
+                    colorPrimary: "#b50d3a",
+                  },
+                  layout: {
+                    logoImageUrl: "/assets/logo.png",
+                    socialButtonsPlacement: "bottom",
+                    
+                  }
+
+               }} path="/" routing="path" signUpUrl="/sign-up" />
             </div>
           </article>
         </main>
