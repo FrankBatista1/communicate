f35d68c049e475d12db4bc07e7f8d5ae8532608f
FrankBatista1 <fb53901@gmail.com>
2023-04-03 12:48:53 -0400

    router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,18 @@
 import { SignOutButton } from "@clerk/nextjs";
 import { type NextPage } from "next";
+import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
+  const { data } = api.posts.getAll.useQuery();
+
   return (
     <>
       <p>an appXD</p>
+      <div>
+        {data?.map((post) => (
+          <div key={post.id}>{post.content}</div>
+        ))}
+      </div>
       <SignOutButton />
     </>
   );
