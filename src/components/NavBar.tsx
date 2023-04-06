07fc74d15629acab5fc8e0fad87c4fa1a6b05bb0
FrankBatista1 <fb53901@gmail.com>
2023-04-05 23:28:56 -0400

    Create post wizard and layout styiling

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,3 +1,4 @@
+import { SignOutButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -5,18 +6,41 @@ interface NavBarProps {
   url: string;
 }
 
-const NavBar: React.FC<NavBarProps> = ( { url } ) => {
+const NavBar: React.FC<NavBarProps> = ({ url }) => {
   return (
-    <div className="h-[100vh] w-[88px] flex justify-center">
-      <div className="fixed flex-col flex justify-between h-[100%] p-3">
+    <div className="flex h-[100vh] w-[88px] justify-center">
+      <div className="fixed flex h-[100%] flex-col justify-between p-3">
         <div className="flex flex-col items-center">
-          <Link className="py-2" href="/"><Image className="w-[100%]"src={"/assets/logowithoutletters.png"} alt="logo" width={40} height={40}/></Link>
-          <Link className="py-2" href="/">Home</Link>
-          <Link className="py-2" href="/">Chat</Link>
-          <Link className="py-2" href="/">Profile</Link>
+          <h1 aria-label="communicate">
+            <Link className="py-2" href="/">
+              <Image
+                className="w-[100%]"
+                src={"/assets/logowithoutletters.png"}
+                alt="logo"
+                width={40}
+                height={40}
+              />
+            </Link>
+          </h1>
+          <Link className="py-2" href="/">
+            Home
+          </Link>
+          <Link className="py-2" href="/">
+            Chat
+          </Link>
+          <Link className="py-2" href="/">
+            Profile
+          </Link>
         </div>
         <div>
-         <Image className="rounded-full w-[100%]" src={url} alt="profile-pic" width={40} height={40}/> 
+          <Image
+            className="w-[100%] rounded-full"
+            src={url}
+            alt="profile-pic"
+            width={40}
+            height={40}
+          />
+          <SignOutButton />
         </div>
       </div>
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,38 @@
-import { SignOutButton, useUser } from "@clerk/nextjs";
+import { useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
 import { api } from "~/utils/api";
 import Head from "next/head";
 import NavBar from "~/components/NavBar";
+import Image from "next/image";
+
+const CreatePostWizdar = () => {
+  const { user } = useUser();
+
+  if (!user) return null;
+
+  return (
+    <div className="flex">
+      <Image
+        src={user.profileImageUrl}
+        className="rounded-full mx-4"
+        alt={"Profile image"}
+        width={40}
+        height={40}
+      />
+      <input
+        placeholder="What's on your mind?"
+        className="bg-transparent grow outline-none"
+      ></input>
+    </div>
+  );
+};
 
 const Home: NextPage = () => {
   const { data } = api.posts.getAll.useQuery();
 
-  const user = useUser();
+  const { user } = useUser();
 
-  const url: string = user.user?.profileImageUrl as string;
+  const imageUrl: string = user?.profileImageUrl as string;
 
   return (
     <>
@@ -21,21 +44,27 @@ const Home: NextPage = () => {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="flex row">
-        <header className="border-solid border-black border-r-2 w-[20%] md:w-[12rem] flex justify-end">
-          <NavBar url={url} />
+      <div className="flex">
+        <header className="flex w-[20%] justify-end border-r-2 border-solid border-black md:w-[12rem]">
+          <NavBar url={imageUrl} />
         </header>
-        <main>
-          <span>Home</span>
+        <main className="grow">
+          <div className="w-[100%] border-b-2 border-black">
+            <h2 className="p-3">Home</h2>
+          </div>
+          <div className="border-b-2 border-black p-4">
+            <CreatePostWizdar />
+          </div>
           <div>
             {data?.map((post) => (
               <div key={post.id}>{post.content}</div>
             ))}
           </div>
-          <SignOutButton />
         </main>
+        <div className="w-[30%] border-l-2 border-black">
+          <h2>Chat</h2>
+        </div>
       </div>
-      
     </>
   );
 };
